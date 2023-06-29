098f0cd75d6373885c241d24ed82d2015b305c1d
rakhimovsh <shukurulloh.com@gmail.com>
2023-06-29 19:53:00 +0500

    add logs

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -3,6 +3,8 @@ import './ProductItem.css'
 
 const ProductItem = ({product}) => {
     const navigate = useNavigate()
+    console.log(product)
+    console.log(product?.image_1)
     return (
         <div className='products-item' onClick={() => navigate('/product/' + product?.id)}>
             <img src={product?.image_1} alt='product image'/>
