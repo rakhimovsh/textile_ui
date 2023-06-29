045e7ad6ce2274823d2769ac840baaa63f877fa9
rakhimovsh <shukurulloh.com@gmail.com>
2023-06-29 09:48:16 +0500

    fixed

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -4,41 +4,17 @@ import './ProductItem.css'
 const ProductItem = ({product}) => {
     const navigate = useNavigate()
     return (
-        <>
-            <div className='products-item' onClick={() => navigate('/product/' + product?.id)}>
-                <img src={'https://picsum.photos/500'} alt='product image'/>
-                <div>
-                    <h4>
-                        {product?.name}
-                    </h4>
-                    <p>
-                        ${product?.price} / 1m²
-                    </p>
-                </div>
+        <div className='products-item' onClick={() => navigate('/product/' + product?.id)}>
+            <img src={'https://picsum.photos/500'} alt='product image'/>
+            <div>
+                <h4>
+                    {product?.name}
+                </h4>
+                <p>
+                    ${product?.price} / 1m²
+                </p>
             </div>
-            <div className='products-item' onClick={() => navigate('/product/' + product?.id)}>
-                <img src={'https://picsum.photos/500'} alt='product image'/>
-                <div>
-                    <h4>
-                        {product?.name}
-                    </h4>
-                    <p>
-                        ${product?.price} / 1m²
-                    </p>
-                </div>
-            </div>
-            <div className='products-item' onClick={() => navigate('/product/' + product?.id)}>
-                <img src={'https://picsum.photos/500'} alt='product image'/>
-                <div>
-                    <h4>
-                        {product?.name}
-                    </h4>
-                    <p>
-                        ${product?.price} / 1m²
-                    </p>
-                </div>
-            </div>
-        </>
+        </div>
     )
 }
 
